409ea36a28e0d562719130de2c8364db5ebbaad3
zach <zach_lawrence_tamu@tamu.edu>
2025-03-27 18:21:56 -0500

    got css to work

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express'), manager = require('./manager.js');
+const path = require('path');
 
 // Create express app
 const app = express();
 const port = 3000;
-	 	 	 	
+
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, 'views')));
 app.use('/', manager);
 
 app.listen(port, () => {
