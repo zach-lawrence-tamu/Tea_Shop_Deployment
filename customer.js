e86eb8d8081bb0193c44a28c9995aa07f1b6fa41
vikrum96 <vikrumravi96@gmail.com>
2025-04-15 17:44:42 -0500

    Added comments to customer_style page

--- customer.js
+++ customer.js
@@ -1,35 +1,3 @@
-// var express = require('express');
-// const { Pool } = require('pg');
-// const dotenv = require('dotenv').config();
-
-// var router = express.Router();
-
-// // Create pool
-// const pool = new Pool({
-//     user: process.env.PSQL_USER,
-//     host: process.env.PSQL_HOST,
-//     database: process.env.PSQL_DATABASE,
-//     password: process.env.PSQL_PASSWORD,
-//     port: process.env.PSQL_PORT,
-//     ssl: {rejectUnauthorized: false}
-// });
-
-// // Add process hook to shutdown pool
-// process.on('SIGINT', function() {
-//     pool.end();
-//     console.log('Application successfully shutdown');
-//     process.exit(0);
-// });
-
-// // / refers to default page index.ejs
-// router.get('/', (req, res) => {
-//     const data = {name: 'Customer page'};
-//     res.render('customer_order_page', data);
-// });
-
-// module.exports = router;
-
-
 var express = require('express');
 const { Pool } = require('pg');
 const dotenv = require('dotenv').config();
