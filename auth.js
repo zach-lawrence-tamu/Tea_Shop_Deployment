f21fdfb8f34008a98a23d52a16630f16db4b546c
zach-lawrence-tamu <157065158+zach-lawrence-tamu@users.noreply.github.com>
2025-04-30 16:52:02 -0500

    Update auth.js

--- auth.js
+++ auth.js
@@ -22,11 +22,16 @@ passport.deserializeUser((user, done) => {
     done(null, user);
 });
 
+const callbackURL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://tea-shop-deployment.onrender.com/auth/google/callback'
+    : 'http://localhost:3000/auth/google/callback';
+
 // Google OAuth strategy
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/auth/google/callback'
+    callbackURL: callbackURL
 },
 async (accessToken, refreshToken, profile, done) => {
     try {
